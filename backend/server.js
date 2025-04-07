7e29c47cddca257558954c06dfd3ba76ee638fca
abdimalik <abdimalik@gmail.com>
2025-04-07 15:19:29 +0300

    Combined both APIs

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,12 @@ import chalk from 'chalk';
 import userRouter from './routes/user.js';
 import cookieParser from 'cookie-parser';
 import postRouter from './routes/post.js';
+import path from 'path'
 
 const app = express();
 const PORT = 8000;
+
+const __dirname = path.resolve()
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,9 +23,15 @@ app.use(cookieParser());
 app.use('/api/user', userRouter);
 app.use('/api/post', postRouter);
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend','dist', 'index.html'));
+    });
+}
 
-connectDB();
 
 app.listen(PORT, () => {
+    connectDB();
     console.log(`${chalk.green.bold('server')} listening on ${PORT}`);
 });
