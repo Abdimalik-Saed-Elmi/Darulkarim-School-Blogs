0e27694cfb366ca9fc1d56778147166ee07d8d9c
abdimalik <abdimalik@gmail.com>
2025-04-07 15:36:14 +0300

    changed one section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Outlet, useNavigate } from "react-router-dom";
 import Header from "./components/Header";
 import { useUser } from "./hooks/useUser";
 import { useEffect } from "react"; // Import useEffect
-import Contact from "./components/aboutus";
+import Contact from "./components/Aboutus";
 import Footer from "./components/Footer";
 
 function App() {
